29a9c6e809a048fd52db05427541b99d993cc3d9
CheezItMan <chris@adadevelopersacademy.org>
2020-09-19 21:27:20 -0700

    Deck initialize method

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,7 +6,12 @@ require_relative 'card'
 class Deck
 
   def initialize
-
+    @cards = []
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards << Card.new(value, suit)
+      end
+    end
   end
 
   def draw

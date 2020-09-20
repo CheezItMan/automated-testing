edab4d2dd861108da91e7fc65a4f1362f7156357
CheezItMan <chris@adadevelopersacademy.org>
2020-09-19 21:07:47 -0700

    add checks for valid arguments in Card constructor

--- lib/card.rb
+++ lib/card.rb
@@ -1,10 +1,21 @@
 
 # card.rb
 
+VALID_SUITS = [:hearts, :diamonds, :clubs, :spades]
+
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
+    unless VALID_SUITS.include? suit
+      raise ArgumentError, "Invalid suit"
+    end
+
+    if value < 1 || value > 13
+      raise ArgumentError, "#{value} is an invalid value"
+    end
+
+
     @value = value
     @suit = suit
     
